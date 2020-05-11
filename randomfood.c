1eab8f39af6deac04c622c65d265ba0021fda442
winith039 <65061103+winith039@users.noreply.github.com>
2020-05-11 12:03:57 +0530

    created a random food list
    prints the food item according to the number

---
+++ randomfood.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main(){
+int choice;
+scanf("%d",&choice);
+switch(choice)
+{
+case 1:
+    printf("Food item- French Fries\nPrice-Rs 99");
+    break;
+case 2:
+    printf("Food item- Pasta\nPrice-Rs 179");
+    break;
+case 3:
+    printf("Food item- Sandwich\nPrice-Rs 149");
+    break;
+case 4:
+    printf("Food item- Burger\nPrice-Rs 129");
+    break;
+case 5:
+    printf("Food item- pizza\nPrice-Rs 339");
+    break;
+}
+}
